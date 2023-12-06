df983939d08d1ad87add58162d6023488d7a27c0
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2023-12-05 19:22:01 -0600

    se agrega el codigo correspondiente

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+const port = process.env.PORT || 3000
+const routes = require("./src/routes/index")
+const db = require("./src/utils/db")
+
+db.connect()
+app.use(express.json())
+
+app.use(morgan("dev"))
+
+app.use("/", routes)
+
+app.use((resp, req, res, next)=>{
+    res.status(resp.status).send(resp.send)
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening in port ${port}`)
+})
