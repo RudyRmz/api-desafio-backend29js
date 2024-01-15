711966a8c4293e6676ec3c49c88be36c67f69796
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-01-14 18:10:31 -0600

    se modifico modelo post para guardar datos del usuari que creo el post, y se añadio mas informacion al token que geneta jwt

--- src/models/posts.js
+++ src/models/posts.js
@@ -1,32 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-//Esquema 
+//Esquema
 const postSchema = new mongoose.Schema({
-    date: { 
-        type: String
-    },
-    dateMiliseconds: {  
-        type: Number
-    },
-    reactions: { 
-        type: Number
-    },
-    description: {
-        required: true,  
-        type: String
-    },
-    title: {
-        required: true,  
-        type: String
-    },
-    url: {
-        required: true,  
-        type: String
-    },
-    tags: {  
-        type: Array
-    }, 
+  date: {
+    type: String,
+  },
+  dateMiliseconds: {
+    type: Number,
+  },
+  reactions: {
+    type: Number,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  title: {
+    required: true,
+    type: String,
+  },
+  url: {
+    required: true,
+    type: String,
+  },
+  tags: {
+    type: Array,
+  },
+  user: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
 });
 
-const Posts =  mongoose.model("Posts", postSchema) // Modelo 
-module.exports = Posts
+const Posts = mongoose.model("Posts", postSchema); // Modelo
+module.exports = Posts;
